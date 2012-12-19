2b7f57df893391faecd68b1a00b6bc615c800d72
Morgan Delahaye-Prat <mdp@arjel.fr>
2012-12-19 16:31:42 +0100

    arch: project reorganization

--- libpip2pi/__init__.py
+++ libpip2pi/__init__.py
@@ -1,1 +1,8 @@
+# -*- encoding: utf-8
+
+
 __version__ = (0, 1, 4)
+
+
+def cli(args):
+    raise NotImplemented('not implemented yet')
--- setup.py
+++ setup.py
@@ -22,12 +22,12 @@ setup(
     author="David Wolever",
     author_email="david@wolever.net",
     description="""
-        pip2pi builds a PyPI-compatible package repository from pip
-        requirements
+    pip2pi builds a PyPI-compatible package repository from pip
+    requirements
     """,
     long_description=long_description,
     packages=find_packages(),
-    scripts=["pip2pi", "pip2tgz", "dir2pi"],
+    scripts=["bin/pip2pi"],
     license="BSD",
     classifiers=[ x.strip() for x in """
         Development Status :: 4 - Beta
